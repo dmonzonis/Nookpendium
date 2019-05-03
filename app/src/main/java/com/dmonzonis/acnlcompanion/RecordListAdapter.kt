5a959bcbaf1a347d9f326be23c564506fe9af41a
monzo94 <dmonzonis@protonmail.com>
2019-05-03 16:23:31 +0200

    Add RecycleView adapter

---
+++ app/src/main/java/com/dmonzonis/acnlcompanion/RecordListAdapter.kt
@@ -0,0 +1,28 @@
+package com.dmonzonis.acnlcompanion
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.record_row.view.*
+
+class RecordListAdapter(private val records: Array<String>) :
+    RecyclerView.Adapter<RecordListAdapter.RecordHolder>() {
+
+    class RecordHolder(v: View) : RecyclerView.ViewHolder(v)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordHolder {
+        val inflatedView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.record_row, parent, false)
+
+        return RecordHolder(inflatedView)
+    }
+
+    override fun getItemCount() = records.size
+
+    override fun onBindViewHolder(holder: RecordHolder, position: Int) {
+        val recordText: String = records[position]
+        holder.itemView.exampleTextView.text = recordText
+    }
+
+}
