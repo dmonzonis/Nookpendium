baca8febfa105c21ff5938453cd3a6023b696103
Elkasitu <beowolfmon_1@hotmail.com>
2020-04-04 21:08:42 +0200

    Implement burger-menu and game-switching mechanism
    This feature implies a lot of changes that were necessary in order to
    achieve the feature-set desired:
    
    * Dropping of appcompat shenanigans for androidx + material design
    * The above implies changing imports and some minimal codebase changes
      in order to achieve the same behaviour as before
    * The base layout is now a DrawerLayout, necessary in order to have a
      navigation drawer (burger menu)
    
    As for the feature itself, these are the changes and additions:
    
    * Burger menu with currently two entries, ACNH and ACNL
    * Both images and datasets for both games
    * Two static tabs for the tabLayout + one created dynamically for ACNL
      (for underwater sea-creatures)
    * A mechanism to change games that will load (or reload, to be
      optimized) the respective dataset and images
    
    In the current implementation, a bug exists wherein switching from ACNH
    to ACNL will not update the UI immediately (a tab has to be chosen and
    then the UI will update) but the opposite does work properly, I blame
    Android.

--- app/src/main/java/com/dmonzonis/nookpendium/CollectionActivity.kt
+++ app/src/main/java/com/dmonzonis/nookpendium/CollectionActivity.kt
@@ -1,37 +1,51 @@
 package com.dmonzonis.nookpendium
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_collection.*
 import java.io.InputStream
 
 class CollectionActivity : AppCompatActivity() {
     private lateinit var viewAdapter: RecordListAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
+    lateinit var drawerToggle: ActionBarDrawerToggle
+    private var selectedGame: Int = R.string.game_acnh
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_collection)
 
-        // Get which collection needs to be opened from the value passed by the main menu activity
-        // TODO: get the selected tab
-        val filename = getString(R.string.fish_file)
+        // Initialize the navigation drawer toggle
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
-        // Read the corresponding XML with the data and fill the records with it
-        val inputStream: InputStream = assets.open(filename)
-        val parser = RecordXmlParser(this)
-        val recordList: List<Record> = parser.parse(inputStream)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navGamesView.setNavigationItemSelectedListener {
+            // FIXME: If another tab other than 0 is selected when changing game, the data
+            // for what normally would be tab 0 will be put into whichever tab is active
+            when (it.itemId) {
+                R.id.miGameAcnh -> changeGame(R.string.game_acnh)
+                R.id.miGameAcnl -> changeGame(R.string.game_acnl)
+            }
+            true
+        }
 
         // Set up the recycler view
         viewManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        viewAdapter = RecordListAdapter(recordList)
-
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = viewManager
-        recyclerView.adapter = viewAdapter
+
+        // Load default game assets (Fish, ACNH)
+        loadGameAssets(tabLayout.getTabAt(0))
 
         tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
             override fun onTabReselected(p0: TabLayout.Tab?) {
@@ -42,16 +56,45 @@ class CollectionActivity : AppCompatActivity() {
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 // TODO: Depending on hemisphere (set in settings), get the NH or SH xml file for ACNH
-                val filename = when (tab?.text.toString()) {
-                    getString(R.string.fish) -> getString(R.string.fish_file)
-                    getString(R.string.bugs) -> getString(R.string.bugs_file)
-                    else -> getString(R.string.underwater_file)
-                }
-                val inputStream: InputStream = assets.open(filename)
-                val recordList: List<Record> = parser.parse(inputStream)
-                viewAdapter = RecordListAdapter(recordList)
-                recyclerView.adapter = viewAdapter
+                loadGameAssets(tab)
             }
         })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun loadGameAssets(tab: TabLayout.Tab?) {
+        val filePath = if (selectedGame == R.string.game_acnh) {
+            if (tabLayout.tabCount > 2) {
+                tabLayout.removeTabAt(2)
+            }
+            when (tab?.text.toString()) {
+                getString(R.string.fish) -> getString(R.string.acnh_fish_nh_path)
+                else -> getString(R.string.acnh_bugs_nh_path)
+            }
+        } else {
+            if (tabLayout.tabCount < 3) {
+                tabLayout.addTab(tabLayout.newTab().setText(R.string.underwater), 2)
+            }
+            when (tab?.text.toString()) {
+                getString(R.string.fish) -> getString(R.string.acnl_fish_path)
+                getString(R.string.bugs) -> getString(R.string.acnl_bugs_path)
+                else -> getString(R.string.acnl_underwater_path)
+            }
+        }
+        val inputStream: InputStream = assets.open(filePath)
+        val recordList = RecordXmlParser(this).parse(inputStream)
+        viewAdapter = RecordListAdapter(recordList)
+        recyclerView.adapter = viewAdapter
+    }
+
+    private fun changeGame(game: Int) {
+        selectedGame = game
+        tabLayout.selectTab(tabLayout.getTabAt(0))
     }
 }
